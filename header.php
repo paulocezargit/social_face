c196efc10339405d0a932343af007b76470b7cd5
Paulo <32718867+paulocezargit@users.noreply.github.com>
2025-04-29 15:46:49 -0300

    Creating login page
    This time, we started creating the login page for our social network. This first step will help us understand how the sign-up and login flow will work, allowing us to map and store the user's initial information to validate their access.

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Social Face</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="icon" type="image/svg+xml" href="assets/images/icon.svg">
 </head>
 <body>
 	
