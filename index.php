bf6f65a8cea7678723d8da1886150d5ea0cef3ac
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-05-04 14:06:09 -0300

    Criando páginas fixas
    Nesta etapa, iniciamos a melhoria da estrutura e do sistema de criação de páginas da nossa rede social. Vamos implementar um mecanismo de carregamento dinâmico de conteúdo através do arquivo index.php, garantindo que as páginas sejam exibidas com mais rapidez, reduzindo atrasos e otimizando o consumo de memória do servidor.

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php include("header.php"); ?>
 
 
+<?php
+
+$url_page = "";
+
+if($url_page == "") {
+
+	include("index.php");
+
+}elseif($url_page == "profile") {
+
+	include("assets/pages/p-profile.php");
+
+}elseif($url_page == "post") {
+
+	include("assets/pages/p-post.php");
+	
+}elseif($url_page == "messages") {
+	
+	include("assets/pages/p-messages.php");
+
+}elseif($url_page == "settings") {
+
+	include("assets/pages/p-settings.php");
+	
+}elseif($url_page == "notifications") {
+
+	include("assets/pages/p-notifications.php");
+	
+}else{
+
+	include("index.php");
+
+}
+
+?>
+
 
 <?php include("footer.php"); ?>
