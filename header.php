f19635da5d23827bccb583568bd281a20d0bcd41
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-06-22 14:17:27 -0300

    Changing Site Routes
    This time, we made some fixes and implementations related to the routes for building our social network. We’ll start by reorganizing the structure to ensure the pages are displayed more quickly and in a well-structured way.

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Social Face</title>
+	<title><?php echo $page_name; ?></title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="icon" type="image/svg+xml" href="assets/images/icon.svg">
 </head>
--- index.php
+++ index.php
@@ -1,41 +1,61 @@
-<?php include("header.php"); ?>
+<?php
+// Pega a URL ou define 'index.php' como padrão
+$url_link = isset($_GET['url']) && !empty($_GET['url']) ? $_GET['url'] : 'index.php';
 
+// Quebra a URL pelos slashes
+$url_link = array_filter(explode('/', $url_link));
+
+// Pega o primeiro item ou 'index' se estiver vazio
+$param1 = isset($url_link[0]) ? $url_link[0] : 'index';
+$param2 = isset($url_link[1]) ? $url_link[1] : null;
+$param3 = isset($url_link[2]) ? $url_link[2] : null;
+
+$url_page = pathinfo($param1, PATHINFO_FILENAME);
+$url_page_2 = pathinfo($param2, PATHINFO_FILENAME);
+$url_page_3 = pathinfo($param3, PATHINFO_FILENAME);
+
+?>
 
 <?php
 
-$url_page = "";
+$site_name = "Social Face";
+$page_name = "";
 
 if($url_page == "") {
-
-	include("index.php");
+    
+    $page_name = "".$site_name;
+	include("assets/pages/p-home.php");
 
 }elseif($url_page == "profile") {
-
+    
+    $page_name = "Profile | ".$site_name;
 	include("assets/pages/p-profile.php");
 
 }elseif($url_page == "post") {
-
+    
+    $page_name = "Post | ".$site_name;
 	include("assets/pages/p-post.php");
 	
 }elseif($url_page == "messages") {
 	
+	$page_name = "Messages | ".$site_name;
 	include("assets/pages/p-messages.php");
 
 }elseif($url_page == "settings") {
-
+    
+    $page_name = "Settings | ".$site_name;
 	include("assets/pages/p-settings.php");
 	
 }elseif($url_page == "notifications") {
-
+    
+    $page_name = "Notifications | ".$site_name;
 	include("assets/pages/p-notifications.php");
 	
 }else{
 
-	include("index.php");
+	$page_name = "".$site_name;
+	include("assets/pages/p-home.php");
 
 }
 
 ?>
-
-
-<?php include("footer.php"); ?>
