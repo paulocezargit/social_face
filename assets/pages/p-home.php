299af7b3d855c0e6dde5ada0a549001905c2f33b
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-07-03 21:14:33 -0300

    Changing structures of the main page
    This time we started changing the structure of the main page to make it simpler and more presentable. We’re going to divide our page into three parts: on the left, there will be the menu and links to other pages; in the center, the main content with the posts; and on the right, other elements will be placed.

--- assets/pages/p-home.php
+++ assets/pages/p-home.php
@@ -1,5 +1,23 @@
 <?php include("header.php"); ?>
 
-Home
+<div class="s-gridpage">
+
+	<div class="s-gridpage-grid">
+
+		<div class="s-gridpage-left">
+
+		</div>
+
+		<div class="s-gridpage-center">
+
+		</div>
+
+		<div class="s-gridpage-right">
+
+		</div>
+
+	</div>
+
+</div>
 
 <?php include("footer.php"); ?>
