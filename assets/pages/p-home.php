38ce203393a6d63260e915ba3a6c76835d20ba15
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2025-07-08 21:10:31 -0300

    Creating more structures
    This time we started creating some more structures for the main page of our social network.

--- assets/pages/p-home.php
+++ assets/pages/p-home.php
@@ -1,6 +1,14 @@
 <?php include("header.php"); ?>
 
-<div class="s-gridpage">
+<section class="s-gridpage">
+
+	<div class="s-menu">
+		<div class="s-menu-box">
+			<div class="s-menu-item">Item 1</div>
+			<div class="s-menu-item">Item 2</div>
+			<div class="s-menu-item">Item 3</div>
+		</div>
+	</div>
 
 	<div class="s-gridpage-grid">
 
@@ -10,6 +18,12 @@
 
 		<div class="s-gridpage-center">
 
+			<div class="s-postinput">
+				<div class="s-postinput-box">
+					<textarea class="s-postinput-textarea" id="s-postinput-textarea" placeholder="Type something…"></textarea>
+				</div>
+			</div>
+
 		</div>
 
 		<div class="s-gridpage-right">
@@ -18,6 +32,6 @@
 
 	</div>
 
-</div>
+</section>
 
 <?php include("footer.php"); ?>
